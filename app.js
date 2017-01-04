648f6f1823fc54a4e09222b3f49dda0f3485e0d1
riyatsai <tly@mobagel.com>
2017-01-05 04:46:28 +0800

    acceleration

--- app.js
+++ app.js
@@ -13,3 +13,17 @@ if(window.DeviceOrientationEvent) {
 }else{
     document.querySelector('body').innerHTML = '你的瀏覽器不支援';
 }
+
+function handleMotionEvent(event) {
+	var x = document.getElementById('x'),
+	y = document.getElementById('y'),
+    z = document.getElementById('z'),
+    accelerationX = event.accelerationIncludingGravity.x,
+    accelerationY = event.accelerationIncludingGravity.y,
+    accelerationZ = event.accelerationIncludingGravity.z;
+	x.innerHTML = accelerationX.toFixed(4);
+	y.innerHTML = accelerationY.toFixed(4);
+	z.innerHTML = accelerationZ.toFixed(4);
+}
+
+window.addEventListener("devicemotion", handleMotionEvent, true);
