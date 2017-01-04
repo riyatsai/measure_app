92f1fabdae638043bf698b4d97868462a664af03
riyatsai <tly@mobagel.com>
2017-01-05 00:45:45 +0800

    to fix

--- app.js
+++ app.js
@@ -6,9 +6,9 @@ if(window.DeviceOrientationEvent) {
           	alpha = event.webkitCompassHeading,
           	beta = event.beta,
           	gamma = event.gamma;
-			a.innerHTML = Math.round(alpha);
-			b.innerHTML = Math.round(beta);
-			g.innerHTML = Math.round(gamma);
+			a.innerHTML = alpha.toFixed(4);
+			b.innerHTML = beta.toFixed(4);
+			g.innerHTML = gamma.toFixed(4);
 		},false);
 }else{
     document.querySelector('body').innerHTML = '你的瀏覽器不支援';
