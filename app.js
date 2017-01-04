2ad0bec00796468dba5a02ad63634cfc5b736198
riyatsai <tly@mobagel.com>
2017-01-05 00:50:40 +0800

    2

--- app.js
+++ app.js
@@ -7,8 +7,8 @@ if(window.DeviceOrientationEvent) {
           	beta = event.beta,
           	gamma = event.gamma;
 			a.innerHTML = alpha.toFixed(4);
-			b.innerHTML = beta.toFixed(4);
-			g.innerHTML = gamma.toFixed(4);
+			b.innerHTML = beta.toFixed(2);
+			g.innerHTML = gamma.toFixed(2);
 		},false);
 }else{
     document.querySelector('body').innerHTML = '你的瀏覽器不支援';
