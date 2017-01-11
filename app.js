35aa304b1e84b4ca8ffb844a3e5479880afcc185
divac lin <divac.d.zoro@gmail.com>
2017-01-11 21:56:51 +0800

    dfsdf
    vcxvcx

--- app.js
+++ app.js
@@ -6,21 +6,21 @@
 	window.st = false;
 })();
 
-if(window.DeviceOrientationEvent) {
-	window.addEventListener('deviceorientation', function(event) {
-		var a = document.getElementById('alpha'),
-			b = document.getElementById('beta'),
-          	g = document.getElementById('gamma'),
-          	alpha = event.webkitCompassHeading,
-          	beta = event.beta,
-          	gamma = event.gamma;
-			a.innerHTML = alpha.toFixed(4);
-			b.innerHTML = beta.toFixed(2);
-			g.innerHTML = gamma.toFixed(2);
-		},false);
-}else{
-    document.querySelector('body').innerHTML = '不支援';
-}
+// if(window.DeviceOrientationEvent) {
+// 	window.addEventListener('deviceorientation', function(event) {
+// 		var a = document.getElementById('alpha'),
+// 			b = document.getElementById('beta'),
+//           	g = document.getElementById('gamma'),
+//           	alpha = event.webkitCompassHeading,
+//           	beta = event.beta,
+//           	gamma = event.gamma;
+// 			a.innerHTML = alpha.toFixed(4);
+// 			b.innerHTML = beta.toFixed(2);
+// 			g.innerHTML = gamma.toFixed(2);
+// 		},false);
+// }else{
+//     document.querySelector('body').innerHTML = '不支援';
+// }
 
 function handleMotionEvent(event) {
 /*	var x = document.getElementById('x'),
